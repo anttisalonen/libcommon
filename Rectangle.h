baa0f4df8b009341a9413bf161f5ee73a8a85d10
Antti Salonen <ajsalonen@gmail.com>
2012-08-12 14:34:43 +0200

    add helper functions

--- Rectangle.h
+++ Rectangle.h
@@ -1,9 +1,12 @@
 #ifndef COMMON_RECTANGLE_H
 #define COMMON_RECTANGLE_H
 
+#include <iostream>
+
 namespace Common {
 
 struct Rectangle {
+	inline Rectangle();
 	inline Rectangle(float x_, float y_, float w_, float h_);
 	inline bool pointWithin(float x_, float y_) const;
 	float x;
@@ -11,6 +14,15 @@ struct Rectangle {
 	float w;
 	float h;
 };
+
+inline std::ostream& operator<<(std::ostream& out, const Rectangle& r)
+{
+	out << "(" << r.x << ", " << r.y << ") - (" << r.x + r.w << ", " << r.y + r.h << ")";
+	return out;
+}
+
+Rectangle::Rectangle()
+	: x(0), y(0), w(0), h(0) { }
 
 Rectangle::Rectangle(float x_, float y_, float w_, float h_)
 	: x(x_), y(y_), w(w_), h(h_)
