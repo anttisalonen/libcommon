1a2e75e38ad44720b1d43ba508ce6bbefa4b593a
Antti Salonen <ajsalonen@gmail.com>
2013-01-03 23:31:13 +0100

    replace Point structure with Vector2

--- CellSpacePartition.h
+++ CellSpacePartition.h
@@ -4,8 +4,11 @@
 #include <list>
 #include <vector>
 #include <cmath>
+#include <cassert>
 
 #include "Math.h"
+#include "Vector2.h"
+#include "Partition.h"
 
 namespace Common {
 
@@ -19,17 +22,18 @@ struct Cell {
 template<class T>
 class CellSpacePartition {
 	public:
+		// maxentities is the maximum number of T's returned by any single query
 		inline CellSpacePartition(float w, float h,
 				unsigned int cellsx, unsigned int cellsy, unsigned int maxentities);
-		inline void add(T& t, const Point& p);
-		inline void remove(T& t, const Point& p);
-		inline void update(T& t, const Point& oldpos, const Point& newpos);
-		inline T& queryBegin(const Point& p, float radius);
+		inline void add(T& t, const Vector2& p);
+		inline void remove(T& t, const Vector2& p);
+		inline void update(T& t, const Vector2& oldpos, const Vector2& newpos);
+		inline T& queryBegin(const Vector2& p, float radius);
 		inline bool queryEnd() const;
 		inline T& queryNext();
 
 	private:
-		inline unsigned int positionToIndex(const Point& p) const;
+		inline unsigned int positionToIndex(const Vector2& p) const;
 		std::vector<Cell<T>> mCells;
 		float mWidth;
 		float mHeight;
@@ -61,9 +65,9 @@ CellSpacePartition<T>::CellSpacePartition(float w, float h, unsigned int cellsx,
 	mCellHeight = ceil(mHeight / (float)mNumCellsY);
 	for(int j = 0; j < mNumCellsY; j++) {
 		for(int i = 0; i < mNumCellsX; i++) {
-			mCells.push_back(Cell<T>(AABB(Point((i + 0.5f) * mCellWidth - mWidth * 0.5f,
+			mCells.push_back(Cell<T>(AABB(Vector2((i + 0.5f) * mCellWidth - mWidth * 0.5f,
 							(j + 0.5f) * mCellHeight - mHeight * 0.5f),
-						Point(mCellWidth * 0.5f,
+						Vector2(mCellWidth * 0.5f,
 							mCellHeight * 0.5f))));
 		}
 	}
@@ -74,21 +78,21 @@ CellSpacePartition<T>::CellSpacePartition(float w, float h, unsigned int cellsx,
 }
 
 template<class T>
-void CellSpacePartition<T>::add(T& t, const Point& p)
+void CellSpacePartition<T>::add(T& t, const Vector2& p)
 {
 	auto i = positionToIndex(p);
 	mCells.at(i).members.push_back(t);
 }
 
 template<class T>
-void CellSpacePartition<T>::remove(T& t, const Point& p)
+void CellSpacePartition<T>::remove(T& t, const Vector2& p)
 {
 	auto i = positionToIndex(p);
 	mCells.at(i).members.remove(t);
 }
 
 template<class T>
-void CellSpacePartition<T>::update(T& t, const Point& oldpos, const Point& newpos)
+void CellSpacePartition<T>::update(T& t, const Vector2& oldpos, const Vector2& newpos)
 {
 	auto i1 = positionToIndex(oldpos);
 	auto i2 = positionToIndex(newpos);
@@ -100,12 +104,12 @@ void CellSpacePartition<T>::update(T& t, const Point& oldpos, const Point& newpo
 }
 
 template<class T>
-T& CellSpacePartition<T>::queryBegin(const Point& p, float radius)
+T& CellSpacePartition<T>::queryBegin(const Vector2& p, float radius)
 {
-	AABB query(p, Point(radius, radius));
+	AABB query(p, Vector2(radius, radius));
 
-	Point minp = Point(clamp(mWidth * -0.5f, p.x - radius, mWidth * 0.5f - 0.01f), clamp(mHeight * -0.5f, p.y - radius, mHeight * 0.5f - 0.01f));
-	Point maxp = Point(clamp(mWidth * -0.5f, p.x + radius, mWidth * 0.5f - 0.01f), clamp(mHeight * -0.5f, p.y + radius, mHeight * 0.5f - 0.01f));
+	Vector2 minp = Vector2(clamp(mWidth * -0.5f, p.x - radius, mWidth * 0.5f - 0.01f), clamp(mHeight * -0.5f, p.y - radius, mHeight * 0.5f - 0.01f));
+	Vector2 maxp = Vector2(clamp(mWidth * -0.5f, p.x + radius, mWidth * 0.5f - 0.01f), clamp(mHeight * -0.5f, p.y + radius, mHeight * 0.5f - 0.01f));
 	unsigned int minx = positionToIndex(minp) % int(mNumCellsY);
 	unsigned int miny = positionToIndex(minp) / int(mNumCellsY);
 
@@ -116,11 +120,9 @@ T& CellSpacePartition<T>::queryBegin(const Point& p, float radius)
 	for(int j = miny; j <= maxy; j++) {
 		for(int i = minx; i <= maxx; i++) {
 			auto& c = mCells.at(j * mNumCellsY + i);
-			if(!c.members.empty()) {
-				for(auto m : c.members) {
-					assert(mQueryResultNum < mQueryResult.size());
-					mQueryResult.at(mQueryResultNum++) = m;
-				}
+			for(auto m : c.members) {
+				assert(mQueryResultNum < mQueryResult.size());
+				mQueryResult.at(mQueryResultNum++) = m;
 			}
 		}
 	}
@@ -143,7 +145,7 @@ T& CellSpacePartition<T>::queryNext()
 }
 
 template<class T>
-unsigned int CellSpacePartition<T>::positionToIndex(const Point& p) const
+unsigned int CellSpacePartition<T>::positionToIndex(const Vector2& p) const
 {
 	int val = (int(p.y + mHeight * 0.5f) / int(mCellHeight)) * mNumCellsY + (int(p.x + mWidth * 0.5f) / int(mCellWidth));
 	return val;
