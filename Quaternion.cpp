3bc7ffb850c465c8a70e159ca92bf0f0a8269d11
Antti Salonen <ajsalonen@gmail.com>
2013-02-25 23:05:58 +0100

    fix bug in quaternion multiply; add some helper functions

--- Quaternion.cpp
+++ Quaternion.cpp
@@ -15,6 +15,17 @@ Quaternion Quaternion::conjugated() const
 	return Quaternion(-x, -y, -z, w);
 }
 
+float Quaternion::norm() const
+{
+	return sqrt(x * x + y * y + z * z + w * w);
+}
+
+Quaternion Quaternion::versor() const
+{
+	float n = norm();
+	return Quaternion(x / n, y / n, z / n, w / n);
+}
+
 Quaternion Quaternion::operator*(const Vector3& v) const
 {
 	float w_ = - (x * v.x) - (y * v.y) - (z * v.z);
@@ -26,11 +37,19 @@ Quaternion Quaternion::operator*(const Vector3& v) const
 
 Quaternion Quaternion::operator*(const Quaternion& q) const
 {
-	float w_ = - (w * q.w) - (x * q.x) - (y * q.y) - (z * q.z);
+	// the ordering is correct
+	float w_ =   (w * q.w) - (x * q.x) - (y * q.y) - (z * q.z);
 	float x_ =   (x * q.w) + (w * q.x) + (y * q.z) - (z * q.y);
 	float y_ =   (y * q.w) + (w * q.y) + (z * q.x) - (x * q.z);
 	float z_ =   (z * q.w) + (w * q.z) + (x * q.y) - (y * q.x);
 	return Quaternion(x_, y_, z_, w_);
+}
+
+Quaternion Quaternion::fromAxisAngle(const Vector3& axis, float angle)
+{
+	float half = angle * 0.5f;
+	float s = sin(half);
+	return Quaternion(s * axis.x, s * axis.y, s * axis.z, cos(half));
 }
 
 
--- Quaternion.h
+++ Quaternion.h
@@ -9,14 +9,23 @@ class Quaternion {
 	public:
 		Quaternion(float x_, float y_, float z_, float w_);
 		Quaternion conjugated() const;
+		float norm() const;
+		Quaternion versor() const;
 		Quaternion operator*(const Vector3& v) const;
 		Quaternion operator*(const Quaternion& q) const;
+		static Quaternion fromAxisAngle(const Vector3& axis, float angle);
 
 		float x;
 		float y;
 		float z;
 		float w;
 };
+
+inline std::ostream& operator<<(std::ostream& out, const Quaternion& q)
+{
+	out << "(" << q.x << ", " << q.y << ", " << q.z << ", " << q.w << ")";
+	return out;
+}
 
 }
 
